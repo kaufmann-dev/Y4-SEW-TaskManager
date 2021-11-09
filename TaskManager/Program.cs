0fffb313e91152263a291e3be2cfc941e41651cd
David Kaufmann <david@kaufmann.dev>
2021-11-09 23:13:33 +0100

    lol

---
+++ TaskManager/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TaskManager
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            TaskManager tm = new TaskManager();
+            await tm.SimulateExecution();
+        }
+    }
+}
---
+++ TaskManager/TaskManager.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskManager
+{
+    public class TaskManager
+    {
+        public async Task SimulateExecution()
+        {
+            SerialExecution();
+            ConcurrentTaskExecution();
+            await TaskExecutionAsync();
+        }
+
+        private void SerialExecution()
+        {
+            var stopWatch = Stopwatch.StartNew();
+            var mSeconds = 0;
+
+            mSeconds += OperationA();
+            mSeconds += OperationB();
+            mSeconds += OperationC();
+            
+            stopWatch.Stop();
+            var elapsedTime = stopWatch.ElapsedMilliseconds;
+            Console.WriteLine($"SerialExecution: serialExecution: {elapsedTime} taskExecution: {mSeconds}");
+        }
+
+        private void ConcurrentTaskExecution()
+        {
+            var stopWatch = Stopwatch.StartNew();
+
+            Task<int> t1 = Task.Run(() => OperationA());
+            Task<int> t2 = Task.Run(OperationB);
+            Task<int> t3 = Task.Run(OperationC);
+
+            var mSeconds = 0;
+
+            var guard = Task.WhenAll(t1, t2, t3);
+            
+            foreach (var i in guard.Result)
+            {
+                mSeconds += i;
+            }
+
+            stopWatch.Stop();
+            var elapsedTime = stopWatch.ElapsedMilliseconds;
+            Console.WriteLine($"ConcurrentTaskExecution: serialExecution: {elapsedTime} taskExecution: {mSeconds}");
+        }
+        
+        private async Task TaskExecutionAsync()
+        {
+            var stopWatch = Stopwatch.StartNew();
+
+            Task<int> t1 = Task.Run(() => OperationA());
+            Task<int> t2 = Task.Run(OperationB);
+            Task<int> t3 = Task.Run(OperationC);
+
+            var mSeconds = 0;
+
+            var guard = await Task.WhenAll(t1, t2, t3);
+            
+            foreach (var i in guard)
+            {
+                mSeconds += i;
+            }
+
+            stopWatch.Stop();
+            var elapsedTime = stopWatch.ElapsedMilliseconds;
+            Console.WriteLine($"TaskExecutionAsync: serialExecution: {elapsedTime} taskExecution: {mSeconds}");
+        }
+        
+        private int OperationA()
+        {
+            Thread.Sleep(700);
+            return 700;
+        }
+        
+        public int OperationB()
+        {
+            Thread.Sleep(500);
+            return 500;
+        }
+        
+        public int OperationC()
+        {
+            Thread.Sleep(680);
+            return 680;
+        }
+    }
+}
